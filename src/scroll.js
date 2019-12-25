e36630bb9117f8a45e662d741c720cb93972a749
Artefakt-ff <bakulev.artyom@yandex.ru>
2019-12-25 21:16:11 +0300

    add files

--- src/scroll.js
+++ src/scroll.js
@@ -1,7 +1,20 @@
 $('a').click(function (e) {
+    if ($.attr(this, 'href').indexOf('#') === -1 && $.attr(this, 'data-lightbox') === undefined) {
+        location.href = $.attr(this, 'href');
+        return
+    }
     e.preventDefault();
     let navbar = document.querySelector('.navbar');
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top - navbar.offsetHeight
     }, 800);
+});
+
+const topButton = $('.top-button');
+document.addEventListener('scroll', function (e) {
+    if (window.scrollY === 0) {
+        topButton.css('opacity', 0);
+    } else {
+        topButton.css('opacity', 1);
+    }
 });
